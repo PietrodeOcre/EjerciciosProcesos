d6288230f1e0feb0de8a5c4945ac29e69896af28
PietrodeOcre <pietrodeocre@gmail.com>
2019-09-30 17:15:39 +0200

    cambios2 30-09-19

--- src/Procesos1/Ejercicio2.java
+++ src/Procesos1/Ejercicio2.java
@@ -1,57 +1,101 @@
 package Procesos1;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.util.Scanner;
 
-import org.w3c.dom.ls.LSException;
-
-//Ejecuta un programa y sale por consola el nombre del proceso ejecutado
-//y el resultado del proceso mediante el InputStream enviado 
-//al InputStreamReader y de ahi al BuffererReader
 public class Ejercicio2 {
 
+	public static String frase;
+
+
+	public static void main(String[] args){
+		
+
+		proceso1();
+
+		
+
+
+	}
+
 	public static void proceso1() {
-		
+
 		ProcessBuilder pb = new ProcessBuilder("bash", "-c", "ls -la");
-		
+
 		try {
 			Process proceso1 = pb.start();
+
+			InputStream is = proceso1.getInputStream();
+
+			InputStreamReader flujo = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(flujo);
 			
+			int letra = 0;
+			try {
+
+				while ((letra =  br.read()) != -1) {
+
+					//letra = is.read();
+					
+					//System.out.print((char)letra);
+					frase = frase + (char)letra;
+
+				}
+				proceso2(frase);
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			}
 			
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
 		
+
 	}
-	
-	public static void proceso2() {
-		
-		ProcessBuilder pb = new ProcessBuilder("bash", "-c", "tr d D");
-		
+
+	public static void proceso2(String frase1) {
+
+		ProcessBuilder pb = new ProcessBuilder("bash", "-c", "echo '"+frase1+"' | tr 'd' 'D'");
+		//echo '"Juan";"Pepe";' | tr '";' ' \t'
 		try {
 			Process proceso2 = pb.start();
 			
+			InputStream is = proceso2.getInputStream();
+
+			InputStreamReader flujo = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(flujo);
 			
-			
+			int letra = 0;
+			try {
+
+				while ((letra =  br.read()) != -1) {
+
+					//letra = is.read();
+					System.out.print((char)letra);
+					
+
+				}
+				
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public static void main(String[] args) {
-		
-		
-		
-	}
+
+
 
 }
