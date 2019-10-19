a361990c92248673de424d2c17f1fa1250c8098a
pietrodeocre <pietrodeocre@pietrodeocre-desktop>
2019-10-19 14:26:57 +0200

    cambio1 19-10-19

--- src/EjercicioHilos1/Ejercicio1.java
+++ src/EjercicioHilos1/Ejercicio1.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 
 public class Ejercicio1 extends Thread{
 	
+	/*
+	 * Defino una variable para guardar los datos 
+	 */
 	private String palabra;
 
 	public static void main(String[] args) {
 		
+		/*
+		 * defino un hilo y lo pongo en marcha
+		 */
 		Thread hilo1= new Ejercicio1(pideString());
 		
 		hilo1.start();
@@ -24,12 +30,24 @@ public class Ejercicio1 extends Thread{
 
 	}
 	
+	/*
+	 * creo un constructor para que el propio hilo
+	 * pudea pedir la palabra usando el metodo
+	 * pideString() con lo que pido por la entrada
+	 * estandar el string que guardo en la variable
+	 */
 	public Ejercicio1(String palabra) {
 		
 		this.palabra = palabra;
 		
 	}
 	
+	 /*
+	  * el metodo pideString nos pide que por la entrada
+	  * estandar escribamos una palabra o frase
+	  * y devuelve esta misma para poder ser usada por el 
+	  * contructor de la clase
+	  */
 	public static String pideString() {
 		
 		String palabra;
@@ -37,7 +55,11 @@ public class Ejercicio1 extends Thread{
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("Escribe una frase: ");
-		
+		/*
+		 * Si se usa el next() para el scanner al encontrar
+		 * un espacio deja de guaradar en el string el 
+		 * resto de lo que se escriba, por eso uso el nextLine()
+		 */
 		palabra = scanner.nextLine();
 		
 		return palabra;
@@ -46,14 +68,27 @@ public class Ejercicio1 extends Thread{
 	
 	public void run() {
 		
-
+		/*
+		 * creamos un array de caracteres auxiliar
+		 * con el tamaño del valor del string dado por
+		 * el metodo anterior
+		 */
 		char[] aux =  new char[palabra.length()];
+		/*
+		 * El contador es para poder aumentar
+		 *  la posicion del array auxiliar
+		 */
 		int cont = 0;
 		for (int i = palabra.length()-1; i >= 0 ; i--){
+			/*
+			 * Recorroel array hacia atras y con el 
+			 * contador avanzo en las posiciones del array aux
+			 * con lo que el string queda guardado alreves
+			 */
 			aux[cont++] = palabra.charAt(i);
 
 		}
-
+		//Imprimo el array auxiliar y aparece como un string normal
 		System.out.println(aux);
 		System.out.println("Soy el hilo hijo.");
 	}
--- src/EjercicioHilos1/Ejercicio2.java
+++ src/EjercicioHilos1/Ejercicio2.java
@@ -5,17 +5,50 @@ import java.util.Scanner;
 
 public class Ejercicio2 extends Thread{
 	
+	/*
+	 * defino las variables que me hacen falta para
+	 * la ejecucion de todos lso hilos
+	 */
 	private String palabra;
 	private int num;
+	
+	private static int numPalabra;
+	
+	private int suma;
 
-	public Ejercicio2(int num) {
+	/*
+	 * Creo un constructor que usara unicamente el hilo 2
+	 */
+	public Ejercicio2(int numPalabra, int num) {
 		this.num=num;
+		/*
+		 * En este constructor llamamos a un metodo
+		 * que suma el numero que ya esta guardado 
+		 * desde que se ejecuto el otro constructor
+		 * es decir el tamaño de la palabra y el numero 
+		 * que le damos desde el metodo pideNum()
+		 */
+		sumar();
 	}
 	
+	/*
+	 * Creo un constructor que usara unicamente el hilo 1
+	 */
 	public Ejercicio2(String palabra) {
 		this.palabra=palabra;
+		/*En el propio contructor doy a una variable 
+		 * definida anteriormente
+		 * el valor de la palabra que recive este 
+		 * contructor mediante el metodo pideCaracteres()
+		 */
+		this.numPalabra = palabra.length();
 	}
 
+	/*
+	 * Este metodo sera usado por el hilo 1
+	 * para pedirnos por la entrada estandar 
+	 * un string
+	 */
 	public static String pideCaracteres() {
 		
 		Scanner scanner = new Scanner(System.in);
@@ -23,11 +56,18 @@ public class Ejercicio2 extends Thread{
 		System.out.print("Escribe una palabra: ");
 		
 		String palabra = scanner.next();
+		//Indico el hilo donde se ejecuta
+		System.out.println("Soy el hilo 1");
 
 		return palabra;
 		
 	}
 	
+	 /*
+	  * Este metodo sera usado por el hilo2
+	  * para pedirnos el numero por la entrada
+	  * estandar 
+	  */
 	public static int pideNum() {
 
 		Scanner scanner = new Scanner(System.in);
@@ -36,28 +76,38 @@ public class Ejercicio2 extends Thread{
 		
 		int num = scanner.nextInt();
 		
-		
 		return num;
 		
 	}
 
 	public static void main(String[] args) {
+		
+		/*
+		 * Definimos los dos hilos cada uno con un constructor diferente
+		 */
 		Thread hilo1= new Ejercicio2(pideCaracteres());
 		
-		Thread hilo2= new Ejercicio2(pideNum());
+		Thread hilo2= new Ejercicio2(numPalabra, pideNum());
 		
 		
-		
+		//Los iniciamos
 		hilo1.start();
 		hilo2.start();
 		try {
+			/*
+			 * queremos que se ejecuten uno detras de otro
+			 * por que de lo contrario si llamamos a suma()
+			 * sinque el primer hilo termine, uno de las variables
+			 * aun estara vacia y valdra cero, con lo que el ejercicio 
+			 * no estara correcto
+			 */
 			hilo1.join();
 			hilo2.join();
 			
 			
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			//e.printStackTrace();
 		}
 		
 		System.out.println("Soy el hilo principal!");
@@ -66,10 +116,32 @@ public class Ejercicio2 extends Thread{
 	
 	public void run() {
 		
+		/*
+		 * Esto se ejecuta en los dos hilos pero como el 
+		 * metodo de la suma solo se ejecuta en el hilo 2
+		 * siempre sera cero hasta que se ejecute el 
+		 * hilo 1 y no le demos el numero al hilo 2
+		 */
+		if (suma != 0) {
+			/*
+			 * Asi que esto no se ejecutará hasta que 
+			 * suma contenga algo es decir hasta que se ejecute 
+			 * en el orden correcto y ocurran todos los pasos
+			 */
+			System.out.println(String.valueOf(suma));
+			System.out.println("Soy el hilo 2");
+		}
 		
-		
-		
-		
+	}
+
+	/*
+	 * Este metodo suma dos atributos de la clase que solo tendran valor 
+	 * definido cuando se ejecute el hilo2 que tiene en su constructor
+	 * la definicion de num mesiente el metodo pideNum() y la ejecucion 
+	 * de este metodo. Dando por fin un valor definico a suma.
+	 */
+	private void sumar() {
+		suma = Integer.parseInt(String.valueOf(numPalabra)) + Integer.parseInt(String.valueOf(num));
 	}
 
 }
