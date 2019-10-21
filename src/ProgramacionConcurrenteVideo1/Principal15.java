11b0a0af70fe20855b3fa9cc9815add5660feb6c
PietrodeOcre <pietrodeocre@gmail.com>
2019-10-21 21:24:33 +0200

    cambios1 21-10-19

--- src/ProgramacionConcurrenteVideo1/Principal15.java
+++ src/ProgramacionConcurrenteVideo1/Principal15.java
@@ -3,6 +3,8 @@ package ProgramacionConcurrenteVideo1;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.omg.CORBA.PRIVATE_MEMBER;
+
 
 /*
  * carrera ciclista 100 ciclistas antes de que comience la siguiente 
@@ -17,14 +19,59 @@ public class Principal15 {
 		
 		int participantes = 100;
 		
+		carrera.setCarrera(participantes);
+		
 		Runtime runtime = Runtime.getRuntime();
 		int nucleos = runtime.availableProcessors();
 		
-		ExecutorService pool = Executors.newFixedThreadPool(nucleos);
+		ExecutorService pool = Executors.newCachedThreadPool();
 		for (int i = 0; i < participantes; i++) {
-			Runnable runnable = new carrera(i, participantes);
+			Runnable runnable = new carrera(i);
 			pool.execute(runnable);
 		}
+		
+		pool.shutdown();
+		while(!pool.isTerminated());
+		
+		double[][] tiempos = carrera.getTiempos();
+		
+		int idGanador = 0;
+		double ganadorTiempo = tiempos[0][3];
+		
+		int idGanador1 = 0;
+		double ganadorTiempo1 = tiempos[0][0];
+		
+		int idGanador2 = 0;
+		double ganadorTiempo2 = tiempos[0][1];
+		
+		int idGanador3 = 0;
+		double ganadorTiempo3 = tiempos[0][2];
+		
+		for(int i = 0; i<tiempos.length; i++) {
+			if(tiempos[i][3]< ganadorTiempo) {
+				ganadorTiempo = tiempos[i][3];
+				idGanador = i;
+			}
+			if(tiempos[i][0] < ganadorTiempo1) {
+				ganadorTiempo1 = tiempos[i][0];
+				idGanador1 = i;
+			}
+			if(tiempos[i][1] < ganadorTiempo2) {
+				ganadorTiempo2 = tiempos[i][1];
+				idGanador2 = i;
+			}
+			if(tiempos[i][2] < ganadorTiempo3) {
+				ganadorTiempo3 = tiempos[i][2];
+				idGanador3 = i;
+			}
+		}
+		
+		
+		System.out.println("El ganador es el hilo: "+ idGanador);
+		System.out.println("El ganador de la Etapa 1 es el hilo: "+ idGanador1);
+		System.out.println("El ganador de la Etapa 2 es el hilo: "+ idGanador2);
+		System.out.println("El ganador de la Etapa 3 es el hilo: "+ idGanador3);
+		
 		
 	}
 	
--- src/ProgramacionConcurrenteVideo1/carrera.java
+++ src/ProgramacionConcurrenteVideo1/carrera.java
@@ -1,19 +1,70 @@
 package ProgramacionConcurrenteVideo1;
 
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
 public class carrera implements Runnable {
 	
 	private int id;
-	private static int participantes;
 
-	public carrera(int i, int participantes) {
+	
+	private double inicio, total;
+	private Random rand = new Random(System.nanoTime());
+
+	private static double[][] tiempos;
+	private static CyclicBarrier barrera;
+	
+	public carrera(int i) {
 		this.id = i;
-		this.participantes = participantes;
+
+	}
+	
+	public static void setCarrera(int participantes) {
+		tiempos = new double[participantes][4];
+		barrera = new CyclicBarrier(participantes);
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-
+		
+		etapa(0);
+		etapa(1);
+		etapa(2);
+		
+		tiempos[id][3] = tiempos[id][0] + tiempos[id][1] + tiempos[id][2];
+		
+	}
+	
+	private void etapa(int numEtapa) {
+		inicio = System.nanoTime();
+		
+		try {
+			Thread.sleep(rand.nextInt(100) + 100);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		total = System.nanoTime() - inicio;
+		
+		tiempos[id][numEtapa] =  total;
+		
+		try {
+			barrera.await();
+			barrera.reset();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (BrokenBarrierException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static double[][] getTiempos(){
+		return tiempos;
 	}
 
 }
